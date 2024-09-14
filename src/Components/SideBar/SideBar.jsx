36b41667ab66d8bb5a1ae7cd70fa435db5f86239
omerbag-9 <omerbagprog@gmail.com>
2024-09-14 17:48:30 +0300

    enhance

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import {  NavLink } from 'react-router-dom'
 import myImg from '../../assets/profilepic.jpg'
 
 export default function SideBar() {
@@ -14,10 +14,10 @@ export default function SideBar() {
       <h6 className='my-2 text-grey'>Web Developer</h6>
       </div>
       <ul className='text-center'>
-        <li className='mt-5'><NavLink to={""} className={  ({isActive})=> isActive == true ? 'opacity-100 text-main ' : 'opacity-75'  }><i class="fa-solid fa-user-tie user mx-2"></i>Home</NavLink></li>
-        <li className='mt-5'><NavLink to={"projects"} className={  ({isActive})=> isActive == true ? 'opacity-100 text-main ' : 'opacity-75'  }><i class="fa-solid fa-list-check project mx-2"></i>Projects</NavLink></li>
-        <li className='mt-5'><NavLink to={"about"} className={  ({isActive})=> isActive == true ? 'opacity-100 text-main ' : 'opacity-75'  }><i class="fa-regular fa-address-card about mx-2"></i>About</NavLink></li>
-        <li className='mt-5'><NavLink to={"contact"} className={  ({isActive})=> isActive == true ? 'opacity-100 text-main ' : 'opacity-75'  }><i class="fa-regular fa-envelope contact mx-2"></i>Contact</NavLink></li>
+        <li className='mt-5'><NavLink to={""} className={  ({isActive})=> isActive === true ? 'opacity-100 text-main ' : 'opacity-75'  }><i class="fa-solid fa-user-tie user mx-2"></i>Home</NavLink></li>
+        <li className='mt-5'><NavLink to={"projects"} className={  ({isActive})=> isActive === true ? 'opacity-100 text-main ' : 'opacity-75'  }><i class="fa-solid fa-list-check project mx-2"></i>Projects</NavLink></li>
+        <li className='mt-5'><NavLink to={"about"} className={  ({isActive})=> isActive === true ? 'opacity-100 text-main ' : 'opacity-75'  }><i class="fa-regular fa-address-card about mx-2"></i>About</NavLink></li>
+        <li className='mt-5'><NavLink to={"contact"} className={  ({isActive})=> isActive === true ? 'opacity-100 text-main ' : 'opacity-75'  }><i class="fa-regular fa-envelope contact mx-2"></i>Contact</NavLink></li>
       </ul>
     </div>
 
